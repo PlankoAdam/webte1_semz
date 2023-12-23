79241124dc3bd98e134d0cd86dea0a4a21042ba3
Adam Planko <adi.planko@gmail.com>
2023-12-23 09:58:03 +0100

    tailwind, pixijs init

---
+++ src/components/MainGame.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-10 h-10 bg-slate-600"></div>
+</template>
+
+<script>
+// import * as PIXI from "pixi.js";
+
+// const app = new PIXI.Application({ width: 600, height: 600 });
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,45 +1,9 @@
 <template>
-  <canvas ref="canv"></canvas>
+  <div class="flex">
+    <MainGame></MainGame>
+  </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
-
-let canvWidth = window.innerWidth;
-let canvHeight = window.innerHeight;
-const canv = ref(null);
-let ctx;
-let ballX = 200;
-let ballY = 100;
-
-onMounted(() => {
-  ctx = canv.value.getContext("2d");
-  canv.value.width = canvWidth;
-  canv.value.height = canvHeight;
-  window.addEventListener("mousemove", (e) => {
-    ballX = e.clientX;
-    ballY = e.clientY;
-  });
-  window.addEventListener("resize", () => {
-    canvWidth = window.innerWidth;
-    canvHeight = window.innerHeight;
-    canv.value.width = canvWidth;
-    canv.value.height = canvHeight;
-  });
-  animate();
-});
-
-function drawBall(ctx, x, y) {
-  const size = 50;
-  ctx.clearRect(0, 0, canvWidth, canvHeight);
-  ctx.beginPath();
-  ctx.arc(x, y, size, 0, 2 * Math.PI);
-  ctx.fillStyle = "orange";
-  ctx.fill();
-}
-
-function animate() {
-  drawBall(ctx, ballX, ballY);
-  requestAnimationFrame(animate);
-}
+import MainGame from "../components/MainGame.vue";
 </script>
